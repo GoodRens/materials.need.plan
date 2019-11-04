5a5371f928729c8c6d13b243bc2bca6e910fb101
MaLongGui <MaLongGui@longguima>
2019-11-04 09:28:03 +0800

    因为cookie不能在不同controller里面使用，故调整controller

--- src/main/java/com/example/demo/controller/MaterialsNeedPlanController.java
+++ src/main/java/com/example/demo/controller/MaterialsNeedPlanController.java
@@ -1,6 +1,9 @@
-package com.example.demo.login.controller;
+package com.example.demo.controller;
+
+import java.util.List;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.common.vo.CommonResultVo;
+import com.example.demo.department.service.IDepartmentService;
+import com.example.demo.department.vo.DepartmentVo;
 import com.example.demo.login.service.IUserInfoService;
 import com.example.demo.login.vo.UserInfoVO;
 
@@ -18,10 +23,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
-@Api(tags = "User Info Controller")
-@RequestMapping("/mnp/user")
+@Api(tags = "Materials Need Plan Controller")
+@RequestMapping("/")
 @CrossOrigin
-public class UserInfoController {
+public class MaterialsNeedPlanController {
 
 	// cookie变量用来装我们的cookies信息
 	private static Cookie cookie;
@@ -29,6 +34,7 @@ public class UserInfoController {
 	@Autowired
 	private IUserInfoService service;
 
+	/******************** 华丽丽的分割线 ***************************/
 	@ApiOperation(value = "用户登录校验", notes = "根据用户用户名检验")
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public CommonResultVo<?> getUser(HttpServletResponse response, @RequestBody UserInfoVO userInfo) {
@@ -57,4 +63,21 @@ public class UserInfoController {
 		return service.insertUser(userInfo);
 	}
 
+	/******************** 华丽丽的分割线 ***************************/
+	@Autowired
+	IDepartmentService departmentService;
+
+	@ApiOperation(value = "获取部门", notes = "存在层级关系")
+	@RequestMapping(value = "/getDepartments", method = RequestMethod.POST)
+	public CommonResultVo<?> getDepartments(HttpServletRequest request, @RequestBody DepartmentVo departmentVo) {
+		return departmentService.getDepartments(request, departmentVo);
+	}
+
+	@ApiOperation(value = "创建部门", notes = "部门名称，部门层级，父部门id必填（无时为0）")
+	@RequestMapping(value = "/createDepartments", method = RequestMethod.POST)
+	public CommonResultVo<?> createDepartments(HttpServletRequest request,
+			@RequestBody List<DepartmentVo> departmentVos) {
+		return null;
+	}
+	/******************** 华丽丽的分割线 ***************************/
 }
