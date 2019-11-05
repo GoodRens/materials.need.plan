814915c38a896418d52a99e26e2ec5c2920158b7
hiLzd <13419535285@163.com>
2019-11-05 19:40:06 +0800

    添加日期格式

--- src/main/java/com/example/demo/common/vo/CommonBaseVo.java
+++ src/main/java/com/example/demo/common/vo/CommonBaseVo.java
@@ -2,6 +2,10 @@ package com.example.demo.common.vo;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -19,9 +23,13 @@ public class CommonBaseVo {
 	/** 创建人 */
 	private int createrBy;
 	/** 创建时间 */
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 	private Date createDate;
 	/** 最后更新人 */
 	private int lastUpdateBy;
 	/** 最后更新时间 */
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 	private Date lastUpdateDate;
 }
